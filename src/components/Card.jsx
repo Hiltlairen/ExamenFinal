06d55ca6f3ce30f48c52f400b1ab57bf5020b340
william <sirjhoswilliam5@gmail.com>
2023-11-30 10:51:54 -0400

    conexion

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+
+function Card() {
+
+  return (
+    <>
+      <div className="titulo">
+        <h3>Exercícios</h3>
+        <span>4</span>
+      </div>
+    </>
+  )
+}
+
+export default Card;
